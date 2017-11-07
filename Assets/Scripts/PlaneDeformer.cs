6b54a1acb17f9e3669daeb7f2e006c866c7e8b55
bonzaiferroni <lukas8@gmail.com>
2017-11-07 16:51:26 -0700

    added experimental windows build

---
+++ Assets/Scripts/PlaneDeformer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Screeps3D {
+    public class PlaneDeformer : MonoBehaviour {
+        private Mesh mesh;
+
+        private void Start() {
+            mesh = GetComponent<MeshFilter>().mesh;
+        }
+        
+        public void Deform(Vector2[] points) {
+        }
+    }
+}
