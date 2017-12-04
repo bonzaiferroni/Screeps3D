36d97ce5011b20877a3af56b869e1c26210ad264
bonzaiferroni <lukas8@gmail.com>
2017-12-03 20:18:49 -0700

    Fatigue Panel

---
+++ Assets/Scripts/Screeps3D/Selection/Subpanels/FatiguePanel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Screeps3D.Selection
+{
+    public class FatiguePanel : Subpanel
+    {
+        [SerializeField] private TextMeshProUGUI label;
+        
+        private Creep _creep;
+
+        public override string Name
+        {
+            get { return "fatigue"; }
+        }
+
+        public override Type ObjectType
+        {
+            get { return typeof(Creep); }
+        }
+
+        public override void Load(RoomObject roomObject)
+        {
+            _creep = roomObject as Creep;
+            _creep.OnDelta += OnDelta;
+            UpdateDisplay();
+        }
+
+        private void OnDelta(JSONObject obj)
+        {
+            var fatigueData = obj["fatigue"];
+            if (fatigueData == null)
+                return;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            label.text = string.Format("Fatigue: {0}", _creep.Fatigue);
+        }
+
+        public override void Unload()
+        {
+            _creep.OnDelta -= OnDelta;
+        }
+    }
+}
