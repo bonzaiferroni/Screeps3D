39a27e4be6d04bfa36cbde11bb6e23242b7bc2fc
trebbettes <t.rebbettes@gmail.com>
2017-12-16 02:06:12 +0000

    Fixes

--- Assets/Scripts/Screeps3D/RoomObjects/Views/WallHeightView.cs
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/WallHeightView.cs
@@ -6,7 +6,7 @@ namespace Screeps3D.RoomObjects.Views
 {
     public class WallHeightView: MonoBehaviour, IObjectViewComponent
     {
-        private static readonly Dictionary<float, float> Heights = new Dictionary<float, float>
+        private static readonly Dictionary<int, float> Scales = new Dictionary<int, float>
         {
             {1, 0.01f},
             {1000000, 0.1f},
@@ -25,22 +25,31 @@ namespace Screeps3D.RoomObjects.Views
         {
             _gameObject = roomObject.View.gameObject;
             _wall = roomObject as IHitpointsObject;
-            SetHeight();
+            SetScale();
         }
 
         public void Delta(JSONObject data)
         {
             if (data.HasField("hits"))
-               SetHeight();
+               SetScale();
             
         }
 
-        private void SetHeight()
+        private void SetScale()
         {
-            var heighest = Heights.First(h => h.Key > _wall.Hits);
-            var lowest = Heights.Last(h => h.Key < _wall.Hits);
+            if (_wall.Hits > 290000000)
+            {
+                SetHeight(2);
+                return;
+            }
+            var heighest = Scales.First(h => h.Key >= _wall.Hits);
+            var lowest = Scales.Last(h => h.Key <= _wall.Hits);
             var factor = (_wall.Hits - lowest.Key) / (heighest.Key - lowest.Key);
-            var height = (heighest.Value - lowest.Value) * factor + lowest.Value;
+            SetHeight((heighest.Value - lowest.Value) * factor + lowest.Value);
+        }
+
+        private void SetHeight(float height)
+        {
             var ls = _gameObject.transform.localScale;
             _gameObject.transform.localScale = new Vector3(ls.x, height, ls.z);
         }
