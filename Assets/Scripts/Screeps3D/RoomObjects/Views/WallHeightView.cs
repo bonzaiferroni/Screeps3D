45c0bb6e3fc1a01e1c8db3b459e80fda59f28265
trebbettes <t.rebbettes@gmail.com>
2017-12-16 01:55:41 +0000

    Rampart / Wall Heights
    The physical height of the gameObject will scale to its hits... based on localScale. Maximum 2 * model height at 300M hits.

---
+++ Assets/Scripts/Screeps3D/RoomObjects/Views/WallHeightView.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Screeps3D.RoomObjects.Views
+{
+    public class WallHeightView: MonoBehaviour, IObjectViewComponent
+    {
+        private static readonly Dictionary<float, float> Heights = new Dictionary<float, float>
+        {
+            {1, 0.01f},
+            {1000000, 0.1f},
+            {10000000, 1},
+            {100000000, 1.5f},
+            {300000000, 2f}
+        };
+
+        private IHitpointsObject _wall;
+        private GameObject _gameObject;
+        public void Init()
+        {
+        }
+
+        public void Load(RoomObject roomObject)
+        {
+            _gameObject = roomObject.View.gameObject;
+            _wall = roomObject as IHitpointsObject;
+            SetHeight();
+        }
+
+        public void Delta(JSONObject data)
+        {
+            if (data.HasField("hits"))
+               SetHeight();
+            
+        }
+
+        private void SetHeight()
+        {
+            var heighest = Heights.First(h => h.Key > _wall.Hits);
+            var lowest = Heights.Last(h => h.Key < _wall.Hits);
+            var factor = (_wall.Hits - lowest.Key) / (heighest.Key - lowest.Key);
+            var height = (heighest.Value - lowest.Value) * factor + lowest.Value;
+            var ls = _gameObject.transform.localScale;
+            _gameObject.transform.localScale = new Vector3(ls.x, height, ls.z);
+        }
+
+        public void Unload(RoomObject roomObject)
+        {
+        }
+    }
+}
